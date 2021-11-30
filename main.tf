662e727efaa139d4a4efa537f91403e51e54a1b0
prudhvibetaque <94161894+prudhvibetaque@users.noreply.github.com>
2021-11-30 13:18:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_instance" "web_server" {
+    ami = "ami-0279c3b3186e54acd"
+    instance_type = "t2.micro"
+#    subnet_id = var.public_subnet[1]
+ #   key_name = "demo"
+ #   security_groups = [var.security_groups]
+
+   
+    
+    tags = {
+        Name = "web_server"
+    }
+}
